2fa7e822b0f85c447581f7b9c7ee7efc8a51537d
Jason Lee <lee.jason930@gmail.com>
2025-07-27 14:50:10 -0500

    playing around, diffusion

---
+++ refiner.py/main.py
@@ -0,0 +1,44 @@
+# https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0
+
+from diffusers import DiffusionPipeline
+import torch
+from torchvision.transforms.functional import to_pil_image
+
+# load both base & refiner
+base = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+)
+base.to("mps")
+refiner = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0",
+    text_encoder_2=base.text_encoder_2,
+    vae=base.vae,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+)
+refiner.to("mps")
+
+# Define how many steps and what % of steps to be run on each experts (80/20) here
+n_steps = 40
+high_noise_frac = 0.8
+
+prompt = "Sleepy girl on couch with laptop opened on a running video"
+
+# run both experts
+image = base(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_end=high_noise_frac,
+    output_type="latent",
+).images
+# If image[0] is a tensor:
+pil_image = to_pil_image(image[0].cpu())
+pil_image.save("base.png")
+image = refiner(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_start=high_noise_frac,
+    image=image,
+).images[0]
+image.save("refined.png")
